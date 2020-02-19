6f5d122c6247f1586a7061f713c2b2d975bb5f60
Joel Strasser <strasser999@gmail.com>
2020-02-19 22:59:02 +0100

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -13,6 +13,7 @@ $pageRange | ForEach-Object {
     # Get the HTML of the page.
     $currentPage = Invoke-WebRequest -Uri $currentUri
 
+    # Set counters
     $iconCounter = 1
     $fileCounter = 1
     
@@ -26,15 +27,22 @@ $pageRange | ForEach-Object {
     
     $links | ForEach-Object {
 
+        # Set the download uri
         $downloadUri = ([string]($_.href) -replace "about:","http://www.iconarchive.com")
+        
+        # Split the uri in parts  
         $splittedUri = [string]($_.href) -split '/'
+        
+        # Get the file name from the splitted uri
         $fileName = $splittedUri.Get($splittedUri.Count - 1)
 
+        # Status output
         Write-Host "Currently on page $currentPage/$($pageRange.Count) and icon $iconCounter/$($iconsPerPage): $(([string]($_.href) -split '/').Get(([string]($_.href) -split '/').Count -1))"
         
+        # Download file
         Invoke-WebRequest -Uri $downloadUri `-OutFile $fileName
 
-        # Icrement the file counter.
+        # Increment the file counter.
         $fileCounter = $fileCounter + 1;
 
         if(($fileCounter - 1) -eq $filesPerIcon) {
